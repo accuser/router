ad49789eb7472ed2f438523e17de625ae6b04892
Matthew Gibbons <mhgibbons@me.com>
2025-08-05 07:46:57 +0000

    Simplified

--- src/lib/response/json.ts
+++ src/lib/response/json.ts
@@ -1,8 +1,1 @@
-export const json = (data?: unknown, { headers, ...init }: ResponseInit = {}) =>
-	new Response(data === undefined ? data : JSON.stringify(data), {
-		...init,
-		headers: {
-			'Content-Type': 'application/json',
-			...headers
-		}
-	});
+export const json = Response.json;
