b4d641fd5dc3eddb47d20b1cefe6fbde61206228
Matthew Gibbons <mhgibbons@me.com>
2025-08-05 07:48:26 +0000

    Simplified

--- src/lib/response/unsupported-media-type.ts
+++ src/lib/response/unsupported-media-type.ts
@@ -1,4 +1,3 @@
 import { error } from './error';
 
-export const unsupportedMediaType = (reason?: unknown) =>
-	error(415, 'Unsupported Media Type', reason);
+export const unsupportedMediaType = (reason?: unknown) => error(415, 'Unsupported Media Type', reason);
