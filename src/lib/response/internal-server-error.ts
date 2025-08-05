129eb17ebccc49ec3fa1e41305141adfe963aa45
Matthew Gibbons <mhgibbons@me.com>
2025-08-05 07:48:19 +0000

    Simplified

--- src/lib/response/internal-server-error.ts
+++ src/lib/response/internal-server-error.ts
@@ -1,4 +1,3 @@
 import { error } from './error';
 
-export const internalServerError = (reason?: unknown) =>
-	error(500, 'Internal Server Error', reason);
+export const internalServerError = (reason?: unknown) => error(500, 'Internal Server Error', reason);
