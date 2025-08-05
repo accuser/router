822864cfe6398c677def5565af169c582adf9470
Matthew Gibbons <mhgibbons@me.com>
2025-08-05 07:46:08 +0000

    Better typing

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,17 +1,13 @@
-import { internalServerError, notFound } from '$lib/response';
+import { internalServerError, notFound } from './response';
+
+const HTTP_METHODS = {
+	all: '[A-Z]+',
+	get: '(?:GET|HEAD)',
+} as const;
 
 type Flatten<T> = {
 	[K in keyof T]: T[K];
 } & {};
-
-type Maybe<T> = T | undefined | null | void;
-
-type MaybePromise<T> = Maybe<T> | Promise<Maybe<T>>;
-
-export interface Platform<Props = unknown> {
-	ctx: Flatten<Omit<ExecutionContext, 'props'> & { props: Props }>;
-	env: Cloudflare.Env;
-}
 
 type RouteParams<T extends string> = Flatten<
 	T extends `${string}:${infer Param}+${infer Rest}`
@@ -23,43 +19,60 @@ type RouteParams<T extends string> = Flatten<
 		: object
 >;
 
-export interface RequestEvent<Route extends string = string, Props = unknown> {
-	params: RouteParams<Route>;
-	platform: Platform<Props>;
+type Maybe<T> = T | undefined | null | void;
+type MaybePromise<T> = Maybe<T> | Promise<Maybe<T>>;
+
+type ValidPathname = `/${string}` | '/';
+
+export interface Platform<Options extends RouterOptions> {
+	ctx: Flatten<Omit<ExecutionContext, 'props'> & { props: ExtractProps<Options> }>;
+	env: ExtractEnv<Options>;
+}
+
+export interface RequestEvent<Options extends RouterOptions, Pathname extends ValidPathname> {
+	params: RouteParams<Pathname>;
+	platform: Platform<Options>;
 	request: Request;
 	url: URL;
 }
 
-export type RequestHandler<Route extends string = string, Props = unknown> = (
-	event: RequestEvent<Route, Props>
+export type DefaultEnv = Cloudflare.Env;
+export type DefaultMethods = 'delete' | 'get' | 'head' | 'options' | 'patch' | 'post' | 'put' | 'trace';
+export type DefaultProps = {};
+
+export type ExtractEnv<T> = T extends { env: infer E extends object } ? E : DefaultEnv;
+export type ExtractProps<T> = T extends { props: infer P extends object } ? P : DefaultProps;
+export type ExtractMethods<T> = T extends { methods: infer M extends string } ? M : DefaultMethods;
+
+export type RouterOptions = {
+	env?: DefaultEnv;
+	props?: DefaultProps;
+	methods?: DefaultMethods;
+};
+
+export type Router<Options extends RouterOptions> = Flatten<{
+	[K in ExtractMethods<Options>]: <Pathname extends ValidPathname>(
+		pathname: Pathname,
+		...handlers: RequestHandler<Options, Pathname>[]
+	) => Router<Options>;
+}>;
+
+export type RequestHandler<Options extends RouterOptions, Pathname extends ValidPathname> = (
+	event: RequestEvent<Options, Pathname>
 ) => MaybePromise<Response>;
 
-type Router<Props = unknown> = Flatten<
-	{
-		fetch: ExportedHandlerFetchHandler<Cloudflare.Env>;
-	} & {
-		[K in 'all' | 'delete' | 'get' | 'options' | 'patch' | 'post' | 'put']: <
-			Route extends string
-		>(
-			route: Route,
-			...handlers: RequestHandler<Route, Props>[]
-		) => Router<Props>;
-	}
->;
-
-export const router = <Props = unknown>({
-	base = '',
+export const router = <Options extends RouterOptions = {}>({
+	base,
 	onError = internalServerError,
 }: {
-	base?: string;
+	base?: ValidPathname;
 	onError?: (error: unknown, request: Request) => Response | Promise<Response>;
 } = {}) => {
-	const routes: { handlers: RequestHandler<string, Props>[]; re: RegExp }[] =
-		[];
+	const routes: { handlers: RequestHandler<Options, ValidPathname>[]; re: RegExp }[] = [];
 
 	return new Proxy(
 		{
-			async fetch(req, env, ctx) {
+			fetch: (async (req, env, ctx) => {
 				const url = new URL(req.url);
 				const route = `${req.method} ${url.pathname}`;
 
@@ -68,67 +81,60 @@ export const router = <Props = unknown>({
 						const match = route.match(re);
 
 						if (match) {
-							const params = match.groups || {};
+							const params = (match.groups || {}) as RouteParams<ValidPathname>;
+							let res: Maybe<Response>;
 
 							for (const handler of handlers) {
-								const res = await handler({
-									params,
-									platform: { ctx, env },
-									request: req,
-									url,
-								});
-
-								if (res instanceof Response) {
+								if (
+									(res = await handler({
+										params,
+										platform: { ctx, env },
+										request: req,
+										url,
+									})) instanceof Response
+								)
 									return req.method === 'HEAD' && res.bodyUsed
-										? new Response(null, res)
+										? new Response(null, { headers: res.headers, status: res.status, statusText: res.statusText })
 										: res;
-								}
 							}
 						}
 					}
 					return notFound();
-				} catch (error) {
-					return onError(error, req);
+				} catch (e) {
+					return e instanceof Response ? e : onError(e, req);
 				}
-			},
-		} as Router<Props>,
+			}) satisfies ExportedHandlerFetchHandler<ExtractEnv<Options>>,
+		},
 		{
-			get(target, p: keyof Router<Props>, receiver) {
-				return p in target
-					? target[p]
-					: <Route extends string>(
-							route: Route,
-							...handlers: RequestHandler<Route, Props>[]
-					  ) => {
+			get: (target, p, receiver) =>
+				p in target
+					? target[p as keyof typeof target]
+					: typeof p === 'string'
+					? <Pathname extends ValidPathname>(pathname: Pathname, ...handlers: RequestHandler<Options, Pathname>[]) => {
 							routes.push({
 								handlers,
 								re: new RegExp(
-									`^${
-										p === 'all'
-											? '[A-Z]+'
-											: p === 'get'
-											? '(?:GET|HEAD)'
-											: p.toUpperCase()
-									} ${
-										(base + route)
-											// Normalize consecutive slashes: //+ -> /
-											.replace(/\/+/g, '/')
-											// Convert catch-all parameters: :param+ -> (?<param>.*)
-											.replace(/:(\w+)\+/g, '(?<$1>.*)')
-											// Convert regular parameters: :param -> (?<param>[^/]+)
-											.replace(/:(\w+)/g, '(?<$1>[^/]+)')
-											// Escape literal dots
-											.replace(/\./g, '\\.')
-											// Convert wildcards: * -> .*
-											.replace(/\*/g, '.*') +
+									`^${HTTP_METHODS[p as keyof typeof HTTP_METHODS] || p.toUpperCase()} ${(base
+										? base + pathname
+										: pathname
+									)
+										// Normalize consecutive slashes: //+ -> /
+										.replace(/\/+/g, '/')
+										// Convert catch-all parameters: :param+ -> (?<param>.*)
+										.replace(/:(\w+)\+/g, '(?<$1>.*)')
+										// Convert regular parameters: :param -> (?<param>[^/]+)
+										.replace(/:(\w+)/g, '(?<$1>[^/]+)')
+										// Escape literal dots
+										.replace(/\./g, '\\.')
+										// Convert wildcards: * -> .*
+										.replace(/\*/g, '.*')
 										// Handle optional trailing slash
-										'/?'
-									}$`
+										.replace(/\/$/, '/?$')}`
 								),
 							});
 							return receiver;
-					  };
-			},
+					  }
+					: undefined,
 		}
-	) as Router<Props>;
+	) as { fetch: ExportedHandlerFetchHandler<ExtractEnv<Options>> } & Router<Options>;
 };
